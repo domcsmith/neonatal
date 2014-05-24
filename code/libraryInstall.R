25e0177b7ff0b8e6f53ed5e74933a1eb90eb45d4
Dominic Smith <ds4212@ic.ac.uk>
2014-05-24 03:52:07 +0100

    Logistic regression code repurposed and simplified

---
+++ code/libraryInstall.R
@@ -0,0 +1,16 @@
+##########################################################################
+# Package installer script for neonatal data code
+##########################################################################
+
+r <- "http://cran.ma.imperial.ac.uk"
+install.packages("gdata", repos=r)
+install.packages("MASS", repos=r)
+install.packages("klaR", repos=r)
+install.packages("glmnet", repos=r)
+install.packages("denstrip", repos=r)
+install.packages("Matrix", repos=r)
+install.packages("fields", repos=r)
+install.packages("e1071", repos=r)
+install.packages("caret", repos=r)
+
+rm(r)
